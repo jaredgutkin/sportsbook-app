3b863ae69d385510e92b9f89f511bc2ff5a2f968
Jared Gutkin <jgutkinsam@gmail.com>
2022-09-15 14:50:17 -0500

    chart set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Graph from './components/Graph';
 
 function App() {
   return (
@@ -6,7 +7,9 @@ function App() {
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
 
-        <div className="grid md:grid-cols-2 gap-4"></div>
+        <div className="grid md:grid-cols-2 gap-4">
+          <Graph />
+        </div>
       </div>
 
     </div>
---
+++ client/src/components/Graph.jsx
@@ -0,0 +1,17 @@
+import { Chart } from 'chart.js'
+
+export default function Graph() {
+  return (
+    <div className="flex justify-content max-w-xs mx-auto">
+        <div className="item">
+            <div className="chart relative">
+                Chart
+            </div>
+
+            <div className="flex flex-col py-10 gap-4">
+
+            </div>
+        </div>
+    </div>
+  )
+}
